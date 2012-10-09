50537b9a0e74d18bf32c2dcd0c0d087ea8aec575
Brandon Burnett <gentoolicious@gmail.com>
2012-10-09 13:18:37 -0700

    Added first revision of redirectorGUI.

--- redirectorGUI.py
+++ redirectorGUI.py
@@ -1,15 +1,73 @@
 #!/usr/bin/python
-import socket, threading
-from Tkinter import *
+import json, re, socket, threading, wx
+from urlparse import urlparse
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
+urllist = []
+index = 0
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
+keepgoing = True
+
 class SocketThread(threading.Thread):
-    def run():
-        while(run==True):
+    def run(self):
+        while(keepgoing==True):
             data, addr = sock.recvfrom(1024)
-           
+            parsed_url = urlparse(data)
+            domain = '{}://{}/'.format(parsed_url[0], parsed_url[1])
+            if domain not in urllist:
+                urllist.append(domain)
+                listbox.Append(domain)
+            else:
+                print '%s already caught.' % domain   
+
+
+app = wx.App()
+
+frame = wx.Frame(None, -1, 'Woodburn SQUID Thinger', size=(1000,1000))
+frame.Show()
+
+def StartThread(event):
+	btnStart.Disable()
+        btnStop.Enable()
+        t = SocketThread()
+	t.start()
+
+def StopThread(event):
+        btnStart.Enable()
+        btnStop.Disable()
+        btnCreatePolicy.Enable()
+        print urllist
+
+def CreatePolicy(event):
+        data = {}
+        data['domains'] = listbox.GetCheckedStrings()
+        if (txtAddress.GetValue() == "" or txtAppName.GetValue() == ""):
+            print "Must enter a value for IP Address and App Name."
+            lblAddress.SetForegroundColour((255,0,0))
+            lblAppName.SetForegroundColour((255,0,0))
+        else:
+            data['AppName'] = txtAppName.GetValue()
+            print 'JSON string: %s' % json.dumps(data)
+
+listbox = wx.CheckListBox(frame, 26, pos=(10,100), size=(450, 750))
+btnStart = wx.Button(frame, label="Start", pos=(0,0))
+btnStart.Bind(wx.EVT_BUTTON, StartThread)
+
+btnStop = wx.Button(frame, label="Stop", pos=(100,0))
+btnStop.Bind(wx.EVT_BUTTON, StopThread)
+btnStop.Disable()
+
+btnCreatePolicy = wx.Button(frame, label="Create Policy", pos=(200,0))
+btnCreatePolicy.Bind(wx.EVT_BUTTON, CreatePolicy)
+btnCreatePolicy.Disable()
+
+lblAddress = wx.StaticText(frame, pos=(10, 35), label="IP Address:")
+txtAddress = wx.TextCtrl(frame, pos=(150, 35), size=(250, 25))
+lblAppName = wx.StaticText(frame, pos=(10, 65), label="App Name:")
+txtAppName = wx.TextCtrl(frame, pos=(150, 65), size=(250, 25))
+
+app.MainLoop()
