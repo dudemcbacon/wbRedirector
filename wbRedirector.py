cb7233a6b1555bfa9dc22d6651ca0273ed3733a4
Brandon Burnett <gentoolicious@gmail.com>
2012-10-10 13:42:15 -0700

    Changed wbRedirectory.py so it would wait for a OK/NO response.

--- wbRedirector.py
+++ wbRedirector.py
@@ -3,9 +3,13 @@ import logging, socket, sys
 
 __NAME__ = "wbRedirector.py"
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT_SEND = 5005
+UDP_PORT_LISTEN = 5006
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_listen = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock_listen.bind((UDP_IP, UDP_PORT_LISTEN))
 
 logging.basicConfig(filename='example.log',level=logging.DEBUG)
 
@@ -17,7 +21,15 @@ while True:
 	list = line.split(' ')
         redirect = '%s\n' % (list[0])
 	logging.debug('Returning %s.' % (redirect))
-        sock.sendto(redirect, (UDP_IP, UDP_PORT))
-        logging.debug('finished sending')
-        sys.stdout.write(redirect)
-	sys.stdout.flush()
+        sock_send.sendto(redirect, (UDP_IP, UDP_PORT_SEND))
+        logging.debug('Waiting for response...')
+        data, addr = sock_listen.recvfrom(1024)
+        logging.debug('Received %s from %s.' % (data, addr))
+        if (data == "OK"):
+            logging.debug('Returning %s' % redirect)
+            sys.stdout.write(redirect)
+	    sys.stdout.flush()
+        elif (data == "NO"):
+            logging.debug('Returning 404\n")
+            sys.stdout.write("404\n")
+            sys.stdout.flush() 
