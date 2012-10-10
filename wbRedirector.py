4fe78d56e6909d21c6d27f978805c483c4694c3c
Brandon Burnett <gentoolicious@gmail.com>
2012-10-10 14:21:48 -0700

    Improved logging.

--- wbRedirector.py
+++ wbRedirector.py
@@ -11,25 +11,25 @@ sock_recv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock_recv.bind((UDP_IP, UDP_PORT_RECV))
 
-logging.basicConfig(filename='example.log',level=logging.DEBUG)
+logging.basicConfig(filename='example.log',level=logging.INFO)
 
-logging.debug('%s called.' % (__NAME__))
+logging.debug('%s started...' % (__NAME__))
 
 while True:
 	line = sys.stdin.readline().strip()
 	logging.debug('Received %s from Squid.' % (line))
 	list = line.split(' ')
-        redirect = '%s\n' % (list[0])
-	logging.debug('Returning %s.' % (redirect))
-        sock_send.sendto(redirect, (UDP_IP, UDP_PORT_SEND))
-        logging.debug('Waiting for response...')
-        data, addr = sock_recv.recvfrom(1024)
-        logging.debug('Received %s from %s.' % (data, addr))
-        if (data == "OK"):
-            logging.debug('Returning %s' % redirect)
-            sys.stdout.write(redirect)
+    redirect = '%s\n' % (list[0])
+    logging.info('Sending %s to GUI.' % (list[0]))
+    sock_send.sendto(redirect, (UDP_IP, UDP_PORT_SEND))
+    logging.info('Waiting for response from GUI...')
+    data, addr = sock_recv.recvfrom(1024)
+    logging.info('Received %s from %s.' % (data, addr))
+    if (data == "OK"):
+        logging.info('Received OK. Returning %s to squid.' % list[0])
+        sys.stdout.write(redirect)
 	    sys.stdout.flush()
-        elif (data == "NO"):
-            logging.debug('Returning 404\n')
-            sys.stdout.write("404\n")
-            sys.stdout.flush() 
+    elif (data == "NO"):
+        logging.info('Received NO. Returning 404 to squid.')
+        sys.stdout.write("404\n")
+        sys.stdout.flush() 
