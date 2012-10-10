7bcb832fbbccdec2bfb91c53bcf3e4a225f7547f
Brandon Burnett <gentoolicious@gmail.com>
2012-10-10 14:24:58 -0700

    Fixed indentation errors.

--- wbRedirector.py
+++ wbRedirector.py
@@ -16,11 +16,11 @@ logging.basicConfig(filename='example.log',level=logging.INFO)
 logging.debug('%s started...' % (__NAME__))
 
 while True:
-	line = sys.stdin.readline().strip()
-	logging.debug('Received %s from Squid.' % (line))
-	list = line.split(' ')
+    line = sys.stdin.readline().strip()
+    logging.debug('Received %s from Squid.' % (line))
+    list = line.split(' ')
     redirect = '%s\n' % (list[0])
-    logging.info('Sending %s to GUI.' % (list[0]))
+    logging.info('Sending %s to GUI.' % list[0])
     sock_send.sendto(redirect, (UDP_IP, UDP_PORT_SEND))
     logging.info('Waiting for response from GUI...')
     data, addr = sock_recv.recvfrom(1024)
@@ -28,7 +28,7 @@ while True:
     if (data == "OK"):
         logging.info('Received OK. Returning %s to squid.' % list[0])
         sys.stdout.write(redirect)
-	    sys.stdout.flush()
+        sys.stdout.flush()
     elif (data == "NO"):
         logging.info('Received NO. Returning 404 to squid.')
         sys.stdout.write("404\n")
